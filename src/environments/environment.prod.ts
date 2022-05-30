b9eff60d9057987accecf6cdb9f960d15ea47c9d
CARLOS ANDRES QUEL REDROBAN <carlos.quel@epn.edu.ec>
2022-05-30 13:44:15 -0500

    Ionic project with Firebase

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  firebase: {
+    projectId: 'ionic-2022a',
+    appId: '1:924789232702:web:f97457b330678b96af2d6f',
+    storageBucket: 'ionic-2022a.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyDb8903AXXPEzmYOyslnnlCv-awYPedWKI',
+    authDomain: 'ionic-2022a.firebaseapp.com',
+    messagingSenderId: '924789232702',
+    measurementId: 'G-JBNZRD5ZRH',
+  },
+  production: true
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,42 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  firebase: {
+    projectId: 'ionic-2022a',
+    appId: '1:924789232702:web:f97457b330678b96af2d6f',
+    storageBucket: 'ionic-2022a.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyDb8903AXXPEzmYOyslnnlCv-awYPedWKI',
+    authDomain: 'ionic-2022a.firebaseapp.com',
+    messagingSenderId: '924789232702',
+    measurementId: 'G-JBNZRD5ZRH',
+  },
+  production: false
+};
+
+/* Techer Environments
+export const environment = {
+  firebase: {
+    projectId: 'proyectos-2022a',
+    appId: '1:631200823484:web:9bff68ef93cc81093bf8b9',
+    storageBucket: 'proyectos-2022a.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyBgQrurAZlRn6wL3SieLcf5Od7gFf2Fcl8',
+    authDomain: 'proyectos-2022a.firebaseapp.com',
+    messagingSenderId: '631200823484',
+    measurementId: 'G-MKCMG9Y4B9'
+  },
+  production: false
+};
+*/
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
